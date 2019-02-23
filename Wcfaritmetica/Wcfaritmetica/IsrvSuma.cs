2fa3e95d28156889461d7d80b9d5a3d17bbda5f9
cech.castellon <47920140+cechgamer@users.noreply.github.com>
2019-02-23 12:10:42 -0400

    se agregó la clase suma

---
+++ Wcfaritmetica/Wcfaritmetica/IsrvSuma.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace Wcfaritmetica
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IService1" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IsrvSuma
+    {
+
+        [OperationContract]
+        clsSuma suma(int numero1, int numero2);
+    }
+
+    [DataContract]
+    public class clsSuma
+    {
+        [DataMember]
+        public int numeroA { get; set; }
+
+        [DataMember]
+        public int numeroB { get; set; }
+
+        [DataMember]
+        public int resultado { get; set; }
+
+
+    }
+}
+
+ 
